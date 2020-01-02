a24c3299db4e97d23594b67dfceddbc054c2ca60
vetwellsoon <shuai168zhu@gmail.com>
2020-01-01 16:53:07 -0800

    faq added, navbar adjusted. dashboard moved to contact

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ const navbar = (
             <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="plans.html">View plans</a>
           </li>
           <li className="nav-item mx-0 mx-lg-1">
-            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="dashboard.html">Faq</a>
+            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="faq.html">Faq</a>
           </li>
           <li className="nav-item mx-0 mx-lg-1">
-            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact us</a>
+            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="dashboard.html">Contact us</a>
           </li>
           <li className="nav-item mx-0 mx-lg-1">
             <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Login</a>
