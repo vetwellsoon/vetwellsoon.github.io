e37ff09adc6a942e93e6cf5f76583d7dda058c9a
vetwellsoon <shuai168zhu@gmail.com>
2020-01-02 16:57:30 -0800

    find-plan pulls all important data

--- form.js
+++ form.js
@@ -1,13 +1,16 @@
 // Not sure to use PHP or Javascript here
 // A $( document ).ready() block.
 
-// don't want to use ?id=
+// document.getElementById("plan-container").style.display = "block"
+// document.getElementsByTagName("body")[0].style.backgroundColor = "#10316B";
+// document.getElementById("main-plan-page").style.display = "none";
+
+// don't want to use ?id=, since we're doing a javascript request
 if (window.location.hash == "#find-plan") {
   document.getElementById("plan-container").style.display = "block"
   document.getElementsByTagName("body")[0].style.backgroundColor = "#10316B";
   document.getElementById("main-plan-page").style.display = "none";
 }
-console.log(window.location.hash)
 document.getElementById('plan-helper').onclick = function(){
   document.getElementById("plan-container").style.display = "block"
   document.getElementsByTagName("body")[0].style.backgroundColor = "#10316B";
@@ -15,12 +18,21 @@ document.getElementById('plan-helper').onclick = function(){
 };
 
 document.getElementById('find-plan-submit').onclick = function(){
+  if (document.getElementById("type-dog").checked) {
+    petType = "dog"
+  } else if (document.getElementById("type-cat").checked) {
+    petType = "cat"
+  }
+
   data = {
-    "pet-name": document.getElementById('pet-name').value,
-    "pet-age": document.getElementById('pet-age').value,
-    "pet-breed": document.getElementById('pet-breed').value
+    "petName": document.getElementById('pet-name').value,
+    "petAge": document.getElementById('pet-age').value,
+    "petBreed": document.getElementById('pet-breed').value,
+    "petType": petType
   }
   console.log(data)
+
+  // set proper plan type here
   document.getElementById("plan-container").style.display = "none"
   document.getElementsByTagName("body")[0].style.backgroundColor = "white";
   document.getElementById("main-plan-page").style.display = "block";
